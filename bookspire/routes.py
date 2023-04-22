e2613afc82e6d2f2489a17f2db668bf86987c125
AnnaSeDab <anna.se.dabrowska@gmail.com>
2023-04-22 14:29:34 +0000

    add: run.py file

--- bookspire/routes.py
+++ bookspire/routes.py
@@ -1,7 +1,10 @@
 from flask import render_template, request, redirect, url_for
-from taskmanager import app, db
-from taskmanager.models import User, Book, Review
+from bookspire import app, db
+from bookspire.models import User, Book, Review
+
 
 @app.route("/")
 def home():
-    return render_template("index.html")
+    return "Hello world"
+
+
