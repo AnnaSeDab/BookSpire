b29dbacc3130fc92b0de2ef718f757f21214d211
AnnaSeDab <anna.se.dabrowska@gmail.com>
2023-04-23 11:53:43 +0000

    add: route for add_book.html to routes.py file

--- bookspire/routes.py
+++ bookspire/routes.py
@@ -8,3 +8,19 @@ def home():
     books = list(Book.query.order_by(Book.id).all())
     return render_template("index.html", books=books)
 
+
+@app.route("/add_book", methods=["GET", "POST"])
+def add_book():
+    if request.method == "POST":
+        book = Book(
+            title=request.form.get("title"),
+            author=request.form.get("author"),
+            year_of_publication=request.form.get("year_of_publication"),
+            synopsis=request.form.get("synopsis"),
+            user_id=request.form.get("user_id"),
+            cover=request.form.get("cover"),
+        )
+        db.session.add(book)
+        db.session.commit()
+        return redirect(url_for("home"))
+    return render_template("add_book.html")
